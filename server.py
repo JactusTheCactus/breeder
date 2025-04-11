a465e08d5b3c8c491de8631d0e3823dbb6914a45
JactusTheCactus <threateninglyamusing@gmail.com>
2025-04-11 01:31:42 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import http.server
 import socketserver
-PORT = 3000
 Handler = http.server.SimpleHTTPRequestHandler
 def serve(port):
     with socketserver.TCPServer(("", port), Handler) as httpd:
-        print("serving at port", f'http://localhost:{port}/')
+        print(f"Serving at http://localhost:{port}/")
         httpd.serve_forever()
-if PORT:
-    serve(PORT)
-else:
-    PORT = int(input('Enter Port #'))
-    serve(PORT)
+try:
+    PORT = int(input("Enter port number (default 3000): ") or 3000)
+except ValueError:
+    print("Invalid input. Using default port 3000.")
+    PORT = 3000
+serve(PORT)
